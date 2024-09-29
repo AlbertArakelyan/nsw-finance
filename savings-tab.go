1d600ed010180cfa107b0f208b84a62b372c2fc1
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-09-29 22:32:22 +0400

    added save btn for updating savings

--- savings-tab.go
+++ savings-tab.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -25,12 +28,34 @@ func (app *App) getSavingsTextContainer() *fyne.Container {
 	amountEntry := widget.NewEntry()
 	amountEntry.SetPlaceHolder(fmt.Sprintf("Amount (%s)", currency))
 	amountEntry.Text = amount
+	amountEntry.Validator = func(s string) error {
+		_, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	amountEntry.OnChanged = func(s string) {
+		_, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			amountEntry.Text = amount
+			return
+		}
+	}
 
 	amountEntryContainer := container.NewBorder(nil, nil, amountText, nil, amountEntry)
 
+	saveBtn := widget.NewButtonWithIcon("Save", theme.DocumentSaveIcon(), func() {})
+	saveBtn.Alignment = widget.ButtonAlignTrailing
+	saveBtn.Importance = widget.HighImportance
+
 	savingsContainer := container.NewVBox(
 		amountEntryContainer,
-		availableAmount,
+		container.NewHBox(
+			availableAmount,
+			layout.NewSpacer(),
+			saveBtn,
+		),
 	)
 	app.uiComponents.SavingsContainer = savingsContainer
 
